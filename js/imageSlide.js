0782dee0f2f567250c64f93ba2d2777823a10d90
chaeiJeon <ahswn1031@gmail.com>
2021-08-14 19:01:12 +0900

    updateCarousel

--- js/imageSlide.js
+++ js/imageSlide.js
@@ -4,26 +4,28 @@ const images = document.querySelector(".images");
 const nextButton = document.querySelector(".nextButton");
 const prevButton = document.querySelector(".prevButton");
 
-//이미지 번호 매기는 flag
-let count = 1;
-
+let count=0;
+// 처음 업로드 된 이미지는 무조건 3번 container에 놓고,
+// 그다음 이미지가 업로드 되면 오른쪽으로 한칸씩 밀린다.
+function addImage(image) {
+    const list = images.children;
+    for (let index = 0; index < list.length; index++) {
+        console.log(list);
+        if(list[index].className==='image1' || list[index].className==='image2'){
+            continue;
+        }
+        else{
+            moveRight(list[index]);
+        }
+    }
+    image.className = `image3`;
+    count++;
+}
 // 이미지 번호 가져오는 함수
 function myNum(item) {
     return parseInt(item.className.replace("image", ""));
 }
-// 처음 업로드 된 이미지는 무조건 3번 container에 놓고,
-// 그다음 이미지가 업로드 되면 오른쪽으로 한칸씩 밀린다.
-function addImage(image) {
-    image.className = `image3`;
-    if(count>1){
-        const list = images.children;
-        for(let index=0; index<list.length;index++){
-            moveRight(list[index]);
-        }
-        image.className = `image3`;
-    }
-    count++;
-}
+// 이미지를 오른쪽으로 한칸 이동하게 하는 함수
 function moveRight(image) {
     image.className = `image${myNum(image) + 1 > 5 ? 1 : myNum(image) + 1}`;
 }
@@ -34,6 +36,10 @@ function moveLeft(image) {
 uploadButton.addEventListener("click", () => { inputButton.click(); })
 // 파일을 가져와서 캐러셀에 올린다
 inputButton.addEventListener("change", (e) => {
+    if(count>=5){
+        alert("이미지는 최대 5장까지 가능합니다!");
+        return;
+    }
     const file = e.target.files[0];
     const reader = new FileReader();
     reader.readAsDataURL(file);
@@ -46,17 +52,31 @@ inputButton.addEventListener("change", (e) => {
 })
 // nextButton을 누르면 각 이미지는 다음 이미지 class로 넘어간다.
 // 6번이 된다면 1번으로 돌아가게 한다
+// next 눌렀는데 4번이 없으면 none
+// prev 눌렀는데 번이 없으면 none
 nextButton.addEventListener("click", event => {
     const list = images.children;
-    for(let index=0; index<list.length;index++){
-        console.log(list[index].className);
-        moveRight(list[index]);
+    for (let index = 0; index < list.length; index++) {
+        if (list[index].className === 'image4') {
+            for (let index = 0; index < list.length; index++) {
+                moveLeft(list[index]);
+            }
+            break;
+        }
     }
 })
 prevButton.addEventListener("click", event => {
+    console.log("prevButton-----------");
     const list = images.children;
-    for(let index=0; index<list.length;index++){
-        console.log(list[index].className);
-        moveLeft(list[index]);
+    for (let index = 0; index < list.length; index++) {
+        console.log(list);
+        if (list[index].className === 'image2') {
+            console.log("moveRight");
+            for (let index = 0; index < list.length; index++) {
+                console.log("for");
+                moveRight(list[index]);
+            }
+            break;
+        }
     }
 })
